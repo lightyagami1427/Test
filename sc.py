330b9d763d44dd3b67269d3736c923b52f1e416c
lightyagami1427 <akhilishtala14@gmail.com>
2025-09-19 14:38:42 +0530

    New Changes

---
+++ sc.py
@@ -0,0 +1,16 @@
+# import re
+
+# text = """diff --git a/secrets.txt b/secrets.txt
+# index f79993c..d6ccb00 100644
+# --- a/secrets.txt
+# +++ b/secrets.txt
+# @@ -1,4 +1,8 @@
+ghp_abc123def456ghi789jkl012mno345pqr678 
+
+ghp_abc123def456ghi789jkl012mno345pqr679
+
+test@gmail.com
+aio_abcdefghijklmnopqrstuvwxyz01 
+# +
+ghp_abc123def456ghi789jkl012mno345pqr678 
+GOOA1234567890123456 
